f2377014841cd68e8acc1786d25019441f6330fb
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-05-15 12:47:22 +0530

    add: hovering cards behind login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import Button from '@/components/custom/button'
 import { signIn } from 'next-auth/react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import AnimatedCards from '@/components/animatedCards'
 
 const LoginPage = () => {
 
@@ -42,52 +43,57 @@ const LoginPage = () => {
 
 
     return (
-        <div className='min-h-screen bg-[url("/herobg.png")] bg-center bg-cover flex items-center justify-center'>
-            <form
-                onSubmit={handleSubmit}
-                className='bg-[#0B0E37] p-8 rounded-lg shadow-lg w-96 flex flex-col gap-6'
-            >
-                <h1 className='text-3xl text-white text-center font-bold'>Welcome Again!</h1>
+        <>
+            <AnimatedCards />
+            <div className='min-h-screen bg-[url("/herobg.png")] bg-center bg-cover'>
+                <div className='absolute flex items-center justify-center w-full h-full backdrop-blur-xs'>
+                    <form
+                        onSubmit={handleSubmit}
+                        className='bg-[#0B0E37] p-8 rounded-lg shadow-lg w-96 flex flex-col gap-6'
+                    >
+                        <h1 className='text-3xl text-white text-center font-bold'>Welcome Again!</h1>
 
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='email' className='text-white font-medium'>
-                        Email
-                    </label>
-                    <input
-                        type='text'
-                        id='email'
-                        name='email'
-                        value={form.email}
-                        onChange={handleChange}
-                        className='p-2 rounded-md bg-[#050A27] text-white outline-none'
-                        required
-                    />
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor='email' className='text-white font-medium'>
+                                Email
+                            </label>
+                            <input
+                                type='text'
+                                id='email'
+                                name='email'
+                                value={form.email}
+                                onChange={handleChange}
+                                className='p-2 rounded-md bg-[#050A27] text-white outline-none'
+                                required
+                            />
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor='password' className='text-white font-medium'>
+                                Password
+                            </label>
+                            <input
+                                type='password'
+                                id='password'
+                                name='password'
+                                value={form.password}
+                                onChange={handleChange}
+                                className='p-2 rounded-md bg-[#050A27] text-white outline-none'
+                                required
+                            />
+                        </div>
+
+                        <Button
+                            type='submit'
+                            className='py-2'
+                        >
+                            Log In
+                        </Button>
+                        <h1 className='text-center'>Need an account? <Link className='underline' href={'/'}>Sign in</Link></h1>
+                    </form>
                 </div>
-
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='password' className='text-white font-medium'>
-                        Password
-                    </label>
-                    <input
-                        type='password'
-                        id='password'
-                        name='password'
-                        value={form.password}
-                        onChange={handleChange}
-                        className='p-2 rounded-md bg-[#050A27] text-white outline-none'
-                        required
-                    />
-                </div>
-
-                <Button
-                    type='submit'
-                    className='py-2'
-                >
-                    Log In
-                </Button>
-                <h1 className='text-center'>Need an account? <Link className='underline' href={'/'}>Sign in</Link></h1>
-            </form>
-        </div>
+            </div>
+        </>
     )
 }
 
