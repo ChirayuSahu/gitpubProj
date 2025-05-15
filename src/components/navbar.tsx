549d71e8133bfef49c0125bf6c0cc1be5e8514e0
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-05-15 12:28:11 +0530

    add: logout button in navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,12 +4,16 @@ import React from 'react'
 import Button from './custom/button'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 
 const Navbar = () => {
 
     const { data: session } = useSession()
     const user = session?.user;
+
+    const router = useRouter()
 
     const navItems = [
         { name: 'Learn', href: '/learn' },
@@ -19,31 +23,40 @@ const Navbar = () => {
     ]
 
     return (
-        <>
-                <div className='fixed z-100 w-full py-4 flex justify-between items-center px-15 text-[#24F2F4] backdrop-blur-md border-b border-[#24F2F4]'>
-                    <div>
-                        <Link href={'/'}>
-                            <h1 className='text-2xl'>{`</CodeClash`}</h1>
-                        </Link>
-                    </div>
-                    <div className='flex gap-15 text-xl'>
-                        {
-                            navItems.map((item) => (
-                                <Link key={item.name} href={item.href}>{item.name}</Link>
-                            ))
-                        }
-                    </div>
-                    {user ? (
-                        <div className=''>
-                            <Button className='text-lg py-2 px-6'>Dashboard</Button>
-                        </div>
-                    ) : (
-                        <div className=''>
-                            <Button className='text-lg py-2 px-6'>Sign Up</Button>
-                        </div>
-                    )}
 
+
+        <>
+            <div className='fixed z-100 w-full py-4 flex justify-between items-center px-15 text-[#24F2F4] backdrop-blur-md border-b border-[#24F2F4]'>
+                <div>
+                    <Link href={'/'}>
+                        <h1 className='text-2xl'>{`</CodeClash`}</h1>
+                    </Link>
                 </div>
+                <div className='flex gap-15 text-xl'>
+                    {
+                        navItems.map((item) => (
+                            <Link key={item.name} href={item.href}>{item.name}</Link>
+                        ))
+                    }
+                </div>
+                {user ? (
+                    <>
+                        <div className='flex gap-2'>
+                            <div className=''>
+                                <Button className='text-lg py-2 px-6' onClick={()=>(router.push('/dashboard'))} >Dashboard</Button>
+                            </div>
+                            <div className=''>
+                                <Button className='text-lg py-2 px-6' onClick={() => signOut()}>Logout</Button>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className=''>
+                        <Button className='text-lg py-2 px-6'>Sign Up</Button>
+                    </div>
+                )}
+
+            </div>
         </>
     )
 }
