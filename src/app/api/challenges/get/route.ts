0d6d5c9ef48a0994f0c2a3a5e44810288344da00
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-05-16 18:55:04 +0530

    fix: get challenges by difficulty and limit.

--- src/app/api/challenges/get/route.ts
+++ src/app/api/challenges/get/route.ts
@@ -6,6 +6,7 @@ export const GET = async (req: NextRequest) => {
   try {
     const { searchParams } = new URL(req.url);
     const difficulty = searchParams.get("difficulty");
+    const limitParam = searchParams.get("limit");
 
     if (!difficulty) {
       return NextResponse.json({ message: "Difficulty is required." }, { status: 400 });
@@ -13,16 +14,25 @@ export const GET = async (req: NextRequest) => {
 
     await connectMongo();
 
-    const randomChallenge = await Challenges.aggregate([
-      { $match: { difficulty } },
-      { $sample: { size: 1 } }
-    ]);
+    // Convert limit to a number if provided
+    const limit = limitParam ? parseInt(limitParam, 10) : null;
 
-    if (randomChallenge.length === 0) {
+    // Use aggregation if limit is provided to get random results
+    let challenges;
+    if (limit) {
+      challenges = await Challenges.aggregate([
+        { $match: { difficulty } },
+        { $sample: { size: limit } }
+      ]);
+    } else {
+      challenges = await Challenges.find({ difficulty });
+    }
+
+    if (challenges.length === 0) {
       return NextResponse.json({ message: "No challenges found for the given difficulty." }, { status: 404 });
     }
 
-    return NextResponse.json({ challenge: randomChallenge[0] }, { status: 200 });
+    return NextResponse.json({ challenges }, { status: 200 });
 
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
