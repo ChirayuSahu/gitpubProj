3f4b017d186598cf23b8c592c58880156cf4893a
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-05-21 18:01:21 +0530

    fix: hero section fixed.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -17,8 +17,8 @@ const Hero = () => {
             <AnimatedCard />
             <div className='min-h-screen bg-[url("/herobg.png")] bg-center'>
                 <div className='absolute flex justify-center w-full h-full backdrop-blur-xs'>
-                    <div className='flex flex-col gap-20 justify-center m-20'>
-                        <h1 className={`text-[#24F2F4] text-5xl sm:text-5xl md:text-6xl font-bold text-center text-shadow-lg/20`}>CodeClash <p>Arena</p></h1>
+                    <div className='flex flex-col gap-10 lg:gap-15 justify-center m-20'>
+                        <h1 className={`text-[#24F2F4] text-5xl lg:text-6xl xl:text-7xl font-bold text-center text-shadow-lg/20`}>CodeClash <p>Arena</p></h1>
                         <div className='text-2xl sm:text-3xl flex gap-2 justify-center items-center'>
                             <h1 className='font-bold text-center text-white text-shadow-lg/20'>Let&apos;s</h1>
                             <RotatingText
@@ -38,7 +38,7 @@ const Hero = () => {
                             <Link
                             href={session ? '/menu' : '/signup'}
                             >
-                            <Button className='px-12 py-4 text-xl sm:text-2xl cursor-pointer'>Start Clashing</Button>
+                            <Button className='px-4 py-2 text-md lg:text-xl lg:px-6 lg:py-4 xl:text-2xl cursor-pointer'>Start Clashing</Button>
                             </Link>
                         </div>
                     </div>
